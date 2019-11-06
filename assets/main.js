3b672f3162c6385f872ced0a9349a4ade591fcf6
Shororo <56631085+Shororo@users.noreply.github.com>
2019-11-06 17:32:20 +0900

    Add files via upload

--- assets/main.js
+++ assets/main.js
@@ -7,3 +7,10 @@ var num  = prompt('あなたの年齢は？');
 var num = Number(num);
 var future = num + 10;
 title.textContent="shoyoさん10年後は" + future + "歳です！";
+
+if(future >= 20){
+	alert("あなたは成人ですね");
+}else{
+	var age = 20 - Number(num);
+	alert(`成人まであと ${age} 歳です`);
+}
