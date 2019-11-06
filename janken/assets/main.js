1477ef4bd058082de89dc8884005559ec2d8c778
Shororo <56631085+Shororo@users.noreply.github.com>
2019-11-06 19:27:09 +0900

    Add files via upload

--- janken/assets/main.js
+++ janken/assets/main.js
@@ -5,19 +5,19 @@ title.classList.add('purple');
 
 var anata = document.getElementById('anata');
 var com = document.getElementById('com');
-var hand = prompt('じゃんけん？　グー、チョキ、パーどれか打って');
+var hand = prompt('じゃんけん？　「グー」、「チョキ」、「パー」どれか打って');
 
 if(hand == "グー"){
 	var num = 0;
-	anata.textContent="貴方：グー";
+	anata.textContent="理沙：グー";
 }else if(hand == "チョキ"){
 	var num = 1;
-	anata.textContent="貴方：チョキ";
+	anata.textContent="理沙：チョキ";
 }else if(hand == "パー"){
 	var num = 2;
-	anata.textContent="貴方：パー";
+	anata.textContent="理沙：パー";
 }else{
-	anata.textContent="貴方：それはない";
+	anata.textContent="理沙：そんな手はないよ";
 }
 
 var anata = Number(num);
@@ -27,19 +27,19 @@ console.log( random );
 
 if(random == 0){
 	var jan = "グー";
-	com.textContent="CPU:グー";
+	com.textContent="翔陽:グー";
 }else if(random == 1){
-	com.textContent="CPU：チョキ";
+	com.textContent="翔陽：チョキ";
 	var jan = "チョキ";
 }else　if(random == 2){
-	com.textContent="CPU:パー";
+	com.textContent="翔陽:パー";
 	var jan = "パー";
 }
 
 if(hand == jan){
-title.textContent="あいこ";
+title.textContent="あいこだね。。。";
 }else if(hand == "グー" && jan == "チョキ" || hand == "チョキ" && jan == "パー" || hand == "パー" && jan == "グー"){
-	title.textContent="貴方の勝ちです";
+	title.textContent="理沙の勝ち！";
 }else{
-	title.textContent="貴方の負けです";
+	title.textContent="理沙の負け！";
 }
